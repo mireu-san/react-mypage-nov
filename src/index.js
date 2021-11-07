ff37ea564b58f76d49c0956eed0bb254394d87fd
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-11-07 23:34:23 +0900

    skeleton part

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// each function or section will be divided to each file later
+
 function Header() {
   return (
   <div className="header__dogma">
       <div className="header__welcome">
         <h1>Welcome, I am Jongwan.</h1>
-        <p>A passionate Frontend Web Developer having an experience of building Web applications with JavaScript, React.js, Node.js with some knowledge of Python, Django and flask.</p>
+        <p>A passionate Frontend Web Developer having an experience of building Web applications with JavaScript, React.js, Node.js including some knowledge of Python, Django and flask.</p>
       </div>
 
       <div className="header__image">
@@ -23,15 +25,44 @@ function Header() {
         <p>Phone:
           <a href="tel:">+82 10 8215 8540</a>
         </p>
-        
-
-
+        {/* add github, linkedin page here */}
       </div>
-
-          
-
   </div>
   );
+}
+
+function Body() {
+  return (
+    <div className="body__dogma">
+      <div className="body__about">
+        <h1>Skills</h1>
+        <p>RATHER TO BE A JACK-OF-ALL-TRADE, I FOCUS ON THE FRONTED FIELD.</p>
+      </div>
+
+      <div className="body__skills">
+        <h1>Proficiency</h1>
+        <p>HTML5, CSS3, JavaScript, React.js, Node.js</p>
+        <p>Python, Django, Flask, MySQL, Git, Github</p>
+      </div>
+
+      <div className="body__projects">
+        <h1>Projects</h1>
+        <p>
+          <a href=""></a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="footer__dogma">
+      <div className="footer__copyright">
+        <p>Copyright © 2020 Jongwan Starmire. All rights reserved.</p>
+      </div>
+    </div>
+  )
 }
 
 
@@ -39,7 +70,8 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
     <Header />
+    <Body />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
